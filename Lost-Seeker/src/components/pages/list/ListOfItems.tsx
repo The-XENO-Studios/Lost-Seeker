93f64ff411f237ec711e3561969e08a8f833cd67
Niklas Ladehoff <business-mail-niklas@web.de>
2023-06-28 16:39:13 +0200

    fixed item list page

--- Lost-Seeker/src/components/pages/list/ListOfItems.tsx
+++ Lost-Seeker/src/components/pages/list/ListOfItems.tsx
@@ -1,41 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Item from "./Item";
-import { getFirestore, collection, getDocs, query, orderBy, limit } from "firebase/firestore"
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+} from "firebase/firestore";
 import { FirebaseApp } from "firebase/app";
 import LandingNavBar from "../landing/components/LandingNavBar";
 import { Navigate } from "react-router-dom";
 
-interface Props{
+interface Props {
   app: FirebaseApp;
   user: any;
 }
 
-const ListOfItems = ({app, user}: Props) => {
-
-  if(!user){
-    return <Navigate to="/"/>
-  }
-
+function ListOfItems({ app, user }: Props) {
   const [finalItem, setFinalItem] = useState<any>();
   const [items, setItems] = useState<any>([]);
 
   const db = getFirestore(app);
-  const ref = collection(db, 'items');
+  const ref = collection(db, "items");
   const q = query(ref, orderBy("time"), limit(10));
-  getDocs(q).then((snapShot)=>{
-    snapShot.docs.forEach((doc)=>{
+
+  getDocs(q).then((snapShot) => {
+    snapShot.docs.forEach((doc) => {
       setItems((currentArray: any) => {
-        return [...currentArray, doc.data]
+        return [...currentArray, doc.data];
       });
       setFinalItem(doc);
-    })
-  })
+    });
+  });
 
   return (
     <div>
       <LandingNavBar />
+
+      {!user ? <Navigate to="/" /> : <></>}
     </div>
-  )
+  );
 }
 
-export default ListOfItems
+export default ListOfItems;
