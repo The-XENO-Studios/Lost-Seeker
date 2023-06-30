d71a20cf8c92a9ccfb67ce4bca46c858ee715045
Niklas Ladehoff <business-mail-niklas@web.de>
2023-06-30 19:12:35 +0200

    added shared logout button

---
+++ Lost-Seeker/src/components/shared/SignOutButton.tsx
@@ -0,0 +1,14 @@
+import { signOut } from "firebase/auth";
+import React from "react";
+import { auth } from "../../App";
+
+export default function SignOutButton({ signOut }: { signOut: () => void }) {
+  return (
+    <button
+      className="bg-black text-white rounded-full w-28 lg:w-32 hidden md:flex items-center justify-center py-1 md:py-2 text-lg font-bold transition-transform hover:scale-95"
+      onClick={signOut}
+    >
+      Sign Out
+    </button>
+  );
+}
