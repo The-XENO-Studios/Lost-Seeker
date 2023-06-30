df1cb890591e9b159d838145a3774c2b23bb9005
Niklas Ladehoff <business-mail-niklas@web.de>
2023-06-30 19:14:29 +0200

    use shared logout

--- Lost-Seeker/src/components/pages/list/ListNavBar.tsx
+++ Lost-Seeker/src/components/pages/list/ListNavBar.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../../../App";
+import SignOutButton from "../../shared/SignoutButton";
 
 function ListNavBar() {
   function SignOut() {
@@ -24,12 +25,7 @@ function ListNavBar() {
           </Link>
         </div>
         {auth.currentUser ? (
-          <div
-            className="bg-white text-text_primary border-2 border-text_primary rounded-full w-28 lg:w-32 flex items-center justify-center py-1 md:py-2 text-lg font-bold transition-transform hover:scale-95"
-            onClick={SignOut}
-          >
-            Sign Out
-          </div>
+          <SignOutButton signOut={SignOut} />
         ) : (
           <div className="flex flex-row items-center gap-3 lg:gap-4">
             <Link
