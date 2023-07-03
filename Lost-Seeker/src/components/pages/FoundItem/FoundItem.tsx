f7431c8dc3ad95b0aab71321eca967953ed3a54b
Niklas Ladehoff <business-mail-niklas@web.de>
2023-07-03 15:47:19 +0200

    added modal and fix use state issue

--- Lost-Seeker/src/components/pages/FoundItem/FoundItem.tsx
+++ Lost-Seeker/src/components/pages/FoundItem/FoundItem.tsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 import NavBar from "../../shared/NavBar";
-import { createContext, useContext, useRef, useState } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 import { db } from "../../../App";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import {
@@ -14,25 +14,16 @@ import "leaflet/dist/leaflet.css";
 import "./Leaflet.css";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import { BiSolidLocationPlus } from "react-icons/bi";
-
+import { latLng, latLngBounds } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import Modal from "./components/Modal";
 interface Props {
   user: any;
 }
 
 function FoundItem({ user }: Props) {
-  if (!user) {
-    return <Navigate to="/register" />;
-  }
-
-  const [nameOfObject, setNameOfObject] = useState();
-  const [place, setPlace] = useState();
-
-  const ref = collection(db, "items");
-
   const [onTop, setOnTop] = useState(false);
-
-  const mapRef = useRef<any>();
-  const mRef = useRef<any>();
+  const [showModal, setShowModal] = useState(false);
 
   /*const docRef = addDoc(ref, {
     nameOfObject: nameOfObject,
@@ -52,65 +43,23 @@ function FoundItem({ user }: Props) {
   };
 
   return (
-    <div>
-      <NavBar onTop={onTop} links={["List", "Contribute"]} />
-      <button
-        className="w-36 h-36 mt-11"
-        onClick={() => {
-          mapRef.current.showModal();
-        }}
-      >
-        Map Button
-      </button>
-      <dialog ref={mapRef} className="bg-transparent">
-        <div
-          className="absolute right-[20px] top-[20px] z-[401] cursor-pointer"
-          onClick={() => {
-            mapRef.current.close();
-          }}
-        >
-          <AiOutlineCloseCircle size={50} />
-        </div>
-        <div
-          className="absolute right-[30px] bottom-[50px] z-[401] cursor-pointer"
-          onClick={() => {
-            mRef.current.locate();
-          }}
-        >
-          <BiSolidLocationPlus size={50} />
-        </div>
-        <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          scrollWheelZoom={true}
-          ref={mRef}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <LocationMarker />
-        </MapContainer>
-      </dialog>
-    </div>
-  );
-}
-
-function LocationMarker() {
-  const [position, setPosition] = useState(null);
-
-  const map = useMapEvents({
-    locationfound(e: any) {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
-    },
-  });
-
-  return position === null ? null : (
     <>
-      <Marker position={position}>
-        <Popup>You are here</Popup>
-      </Marker>
+      {user ? (
+        <>
+          {showModal && <Modal hideModal={() => setShowModal(false)} />}
+          <>
+            <NavBar onTop={onTop} links={["List", "Contribute"]} />
+            <button
+              className="w-36 h-36 mt-11"
+              onClick={() => setShowModal(true)}
+            >
+              Map Button
+            </button>
+          </>
+        </>
+      ) : (
+        <Navigate to="/register" />
+      )}
     </>
   );
 }
