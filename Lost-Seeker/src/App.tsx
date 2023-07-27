af38f45ae372b998883071fffa47de3f6d65ca33
Niklas Ladehoff <business-mail-niklas@web.de>
2023-07-27 18:12:21 +0200

    add questions and answers page

--- Lost-Seeker/src/App.tsx
+++ Lost-Seeker/src/App.tsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import LandingPage from "./components/pages/landing/LandingPage.tsx"
-import LoginPage from "./components/pages/auth/AuthPage.tsx"
-import ErrorPage from "./components/pages/error/ErrorPage.tsx"
-import { initializeApp } from "firebase/app"
-import { getAuth, onAuthStateChanged } from "firebase/auth"
-import ListOfItems from "./components/pages/list/ListOfItems.tsx"
-import FoundItem from "./components/pages/foundItem/FoundItem.tsx"
-import { useState } from "react"
-import { getFirestore } from "firebase/firestore"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/pages/landing/LandingPage.tsx";
+import LoginPage from "./components/pages/auth/AuthPage.tsx";
+import ErrorPage from "./components/pages/error/ErrorPage.tsx";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import ListOfItems from "./components/pages/list/ListOfItems.tsx";
+import FoundItem from "./components/pages/foundItem/FoundItem.tsx";
+import { useState } from "react";
+import { getFirestore } from "firebase/firestore";
+import QAndA from "./components/pages/questions_answers/QAndA.tsx";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB2DxDn0VT7kCGQZKmeXmKmG5zWcM8qHSA",
@@ -17,24 +18,24 @@ const firebaseConfig = {
   messagingSenderId: "727937133568",
   appId: "1:727937133568:web:12028899a4907ffa10fce5",
   measurementId: "G-Z3J14HNCXK",
-}
+};
 
 // Initialize Firebase
 
-const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 function App() {
-  const [user, setUser] = useState<any>()
+  const [user, setUser] = useState<any>();
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
-      setUser(user)
+      setUser(user);
     } else {
-      setUser(null)
+      setUser(null);
     }
-  })
+  });
 
   return (
     <BrowserRouter>
@@ -44,10 +45,11 @@ function App() {
         <Route path="/register" element={<LoginPage />} />
         <Route path="/list" element={<ListOfItems />} />
         <Route path="/foundreport" element={<FoundItem user={user} />} />
+        <Route path="/questions" element={<QAndA />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
